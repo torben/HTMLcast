a8e9e430945dd40c7cb789a81c58a02e21432e99
Torben Toepper <shady@Toeppermanns-MacBook-Pro-3.local>
2011-05-03 00:24:37 +0200

    First Rails Version with Device

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HTMLcast::Application.config.secret_token = '2039357ef92b13c9ebde6b6fa1d1b3eb767ce05982d0c22bee018cca37482d012ffb3bf952944fd586d67546523c6ce28288961b5a16bf7d23a122eb796b8c8f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HTMLcast::Application.config.session_store :cookie_store, :key => '_HTMLcast_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# HTMLcast::Application.config.session_store :active_record_store
