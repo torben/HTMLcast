b8a231dad58eba0b31a6401763bcdaa8aa5e77e7
Torben Toepper <shady@Toeppermanns-MacBook-Pro-3.local>
2011-05-03 20:11:21 +0200

    validation & translation

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '3.0.7'
 
 gem 'mysql'
 gem 'devise'
+gem 'client_side_validations'
 
 # Use unicorn as the web server
 # gem 'unicorn'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_filter :set_locale
+
   protect_from_forgery
+
+  def set_locale
+    I18n.locale = params[:locale]
+  end
 end
