d70c96edd4336bde428f1c986387c4d1418037cf
Torben Toepper <shady@Toeppermanns-MacBook-Pro-3.local>
2011-05-03 20:44:37 +0200

    i18n

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,8 @@ module HTMLcast
     config.time_zone = 'Berlin'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    config.i18n.load_path += Dir[Rails.root.join('locales', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
+    y config.i18n.load_path
     config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
