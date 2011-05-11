565e0fe5232b6a4917446dda42ef02a76046a0a1
Torben Toepper <shady@Toeppermanns-MacBook-Pro-3.local>
2011-05-11 19:50:27 +0200

    welcome gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 require 'uri'
 module ApplicationHelper
+  def avatar_url(email)
+    #default_url = "#{root_url}images/guest.png" ##{CGI.escape(default_url)}
+    gravatar_id = Digest::MD5.hexdigest(email.downcase)
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=mm"
+  end
 end
