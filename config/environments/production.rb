261858dcb3343ced7a9d65d442dcc9c126712fec
Torben Toepper <tt@htmlcast.de>
2011-09-11 20:59:08 +0200

    fix?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ HTMLcast::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true #false
 
   # Compress both stylesheets and JavaScripts
   config.assets.js_compressor  = :uglifier
@@ -54,7 +54,7 @@ HTMLcast::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true #false
 
   # Generate digests for assets URLs
   config.assets.digest = true
